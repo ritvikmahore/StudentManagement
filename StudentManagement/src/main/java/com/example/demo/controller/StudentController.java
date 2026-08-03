67b4d4160575a48cfc99432368075bc6586a5b3f
ritvikmahore <ritvikmahore65@gmail.com>
2026-08-03 10:32:57 +0530

    story 1:url changed

--- StudentManagement/src/main/java/com/example/demo/controller/StudentController.java
+++ StudentManagement/src/main/java/com/example/demo/controller/StudentController.java
@@ -17,7 +17,7 @@ public class StudentController {
 @Autowired
 StudentService ss;
 
-@PostMapping("addStudent")
+@PostMapping("addStudents")
 ResponseEntity addStudent(@RequestBody StudentDto sd) {
 	
 	ss.saveStudent(sd);
